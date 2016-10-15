40931d8d84caf99841bb41b56165f9c6ab813546
Lucas <lucasflima25@gmail.com>
2016-10-15 18:39:59 -0300

    Adicionado Huffman Coding

--- mdl.cpp
+++ mdl.cpp
@@ -1,6 +1,7 @@
 #include "mdl.h"
+#include <iostream>
 
-
+//--------------- Pogramação dinamica ------------------
 int max(int a, int b)
 {
 	return a>b? a:b;
@@ -54,9 +55,13 @@ bool SSP(std::vector<int> set, int sum)
 
 	return subset[sum][n];
 }
+/*
 
-int DDP(std::string str1, std::string str2, int m, int n)
+*/
+int DDP(std::string str1, std::string str2)
 {
+	int m = str1.size();
+	int n = str2.size();
     int dp[m+1][n+1];    
     for (int i=0; i<=m; i++)
         for (int j=0; j<=n; j++)      
@@ -67,3 +72,141 @@ int DDP(std::string str1, std::string str2, int m, int n)
 
     return dp[m][n];
 }
+
+//--------------- Algoritmos gulosos ------------------
+MinHeapNode* newNode(char data, unsigned freq)
+{
+    MinHeapNode* temp = new MinHeapNode();
+    temp->left = temp->right = NULL;
+    temp->data = data;
+    temp->freq = freq;
+    return temp;
+}
+ 
+// A utility function to create a min heap of given capacity
+MinHeap* createMinHeap(unsigned capacity)
+{
+    MinHeap* minHeap = new MinHeap();
+    minHeap->size = 0;  // current size is 0
+    minHeap->capacity = capacity;
+    minHeap->array = new MinHeapNode*[minHeap->capacity];
+    return minHeap;
+}
+ 
+// A utility function to swap two min heap nodes
+void swapMinHeapNode(MinHeapNode** a, MinHeapNode** b)
+{
+    MinHeapNode* t = *a;
+    *a = *b;
+    *b = t;
+}
+ 
+// The standard minHeapify function.
+void minHeapify(MinHeap* minHeap, int idx)
+{
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+ 
+    if (left < minHeap->size &&
+        minHeap->array[left]->freq < minHeap->array[smallest]->freq)
+      smallest = left;
+ 
+    if (right < minHeap->size &&
+        minHeap->array[right]->freq < minHeap->array[smallest]->freq)
+      smallest = right;
+ 
+    if (smallest != idx)
+    {
+        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+        minHeapify(minHeap, smallest);
+    }
+}
+ 
+// A utility function to check if size of heap is 1 or not
+int isSizeOne(MinHeap* minHeap)
+{
+    return (minHeap->size == 1);
+}
+ 
+// A standard function to extract minimum value node from heap
+MinHeapNode* extractMin(MinHeap* minHeap)
+{
+    MinHeapNode* temp = minHeap->array[0];
+    minHeap->array[0] = minHeap->array[minHeap->size - 1];
+    --minHeap->size;
+    minHeapify(minHeap, 0);
+    return temp;
+}
+ 
+// A utility function to insert a new node to Min Heap
+void insertMinHeap(MinHeap* minHeap, MinHeapNode* minHeapNode)
+{
+    ++minHeap->size;
+    int i = minHeap->size - 1;
+    while (i && minHeapNode->freq < minHeap->array[(i - 1)/2]->freq)
+    {
+        minHeap->array[i] = minHeap->array[(i - 1)/2];
+        i = (i - 1)/2;
+    }
+    minHeap->array[i] = minHeapNode;
+}
+ 
+// A standard funvtion to build min heap
+void buildMinHeap(MinHeap* minHeap)
+{
+    int n = minHeap->size - 1;
+    int i;
+    for (i = (n - 1) / 2; i >= 0; --i)
+        minHeapify(minHeap, i);
+}
+ 
+
+// Utility function to check if this node is leaf
+int isLeaf(MinHeapNode* root)
+{
+    return !(root->left) && !(root->right) ;
+}
+ 
+// Creates a min heap of capacity equal to size and inserts all character of 
+// data[] in min heap. Initially size of min heap is equal to capacity
+MinHeap* createAndBuildMinHeap(char data[], int freq[], int size)
+{
+    MinHeap* minHeap = createMinHeap(size);
+    for (int i = 0; i < size; ++i)
+        minHeap->array[i] = newNode(data[i], freq[i]);
+    minHeap->size = size;
+    buildMinHeap(minHeap);
+    return minHeap;
+}
+ 
+// The main function that builds Huffman tree
+MinHeapNode* buildHuffmanTree(char data[], int freq[], int size)
+{
+    MinHeapNode *left, *right, *top;
+ 
+    // Step 1: Create a min heap of capacity equal to size.  Initially, there are
+    // modes equal to size.
+    MinHeap* minHeap = createAndBuildMinHeap(data, freq, size);
+ 
+    // Iterate while size of heap doesn't become 1
+    while (!isSizeOne(minHeap))
+    {
+        // Step 2: Extract the two minimum freq items from min heap
+        left = extractMin(minHeap);
+        right = extractMin(minHeap);
+ 
+        // Step 3:  Create a new internal node with frequency equal to the
+        // sum of the two nodes frequencies. Make the two extracted node as
+        // left and right children of this new node. Add this node to the min heap
+        // '$' is a special value for internal nodes, not used
+        top = newNode('$', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+        insertMinHeap(minHeap, top);
+    }
+ 
+    // Step 4: The remaining node is the root node and the tree is complete.
+    return extractMin(minHeap);
+}
+  
--- mdl.h
+++ mdl.h
@@ -3,6 +3,9 @@
 
 #ifndef __MDL_H__
 #define __MDL_H__
+
+#define MAX_TREE_HT 100
+//--------------- Pogramação dinamica ------------------
 /*
 Retorna o maior entre dois inteiros.
 Parametros: Numeros a serem comparados.
@@ -32,8 +35,66 @@ Parametros: Vector com conjunto de inteiros positivos e soma que deseja ser
 bool SSP(std::vector<int> , int);
 
 /*
+Encontra o número mínimo de operações necessárias para transformar um string no outro.
+Sendo essas oeprações: adição, remoção e substituíção
 
+Parametros: Strings a serem comparadas.
 */
-int DDP(std::string, std::string, int, int);
+int DDP(std::string, std::string);
+
+//--------------- Algoritmos gulosos ------------------
+
+
+typedef struct MinHeapNode
+{
+    char data;  // One of the input characters
+    unsigned freq;  // Frequency of the character
+    MinHeapNode *left, *right; // Left and right child of this node
+} MinHeapNode;
+ 
+// A Min Heap:  Collection of min heap (or Hufmman tree) nodes
+typedef struct MinHeap
+{
+    unsigned size;    // Current size of min heap
+    unsigned capacity;   // capacity of min heap
+    MinHeapNode **array;  // Attay of minheap node pointers
+} MinHeap;
+
+// A utility function allocate a new min heap node with given character
+// and frequency of the character
+MinHeapNode* newNode(char data, unsigned freq);
+
+// A utility function to create a min heap of given capacity
+MinHeap* createMinHeap(unsigned capacity);
+
+// A utility function to swap two min heap nodes
+void swapMinHeapNode(struct MinHeapNode** a, struct MinHeapNode** b);
+
+// The standard minHeapify function.
+void minHeapify(struct MinHeap* minHeap, int idx);
+
+// A utility function to check if size of heap is 1 or not
+int isSizeOne(struct MinHeap* minHeap);
+
+// A standard function to extract minimum value node from heap
+MinHeapNode* extractMin(struct MinHeap* minHeap);
+
+// A utility function to insert a new node to Min Heap
+void insertMinHeap(struct MinHeap* minHeap, struct MinHeapNode* minHeapNode);
+
+// A standard funvtion to build min heap
+void buildMinHeap(struct MinHeap* minHeap);
+
+// Utility function to check if this node is leaf
+int isLeaf(struct MinHeapNode* root);
+
+// Creates a min heap of capacity equal to size and inserts all character of 
+// data[] in min heap. Initially size of min heap is equal to capacity
+MinHeap* createAndBuildMinHeap(char data[], int freq[], int size);
+
+// The main function that builds Huffman tree
+MinHeapNode* buildHuffmanTree(char data[], int freq[], int size);
+
+void HuffmanCodes(char data[], int freq[], int size);
 
 #endif //__MDL_H_
